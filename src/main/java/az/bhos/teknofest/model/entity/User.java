fe71f541252c33bd9d063dd034d4a7fd002569e4
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-03 01:55:49 +0400

    feature: add users table and entity, modify swagger configuration

---
+++ src/main/java/az/bhos/teknofest/model/entity/User.java
@@ -0,0 +1,63 @@
+package az.bhos.teknofest.model.entity;
+
+import az.bhos.teknofest.utils.enums.UserRole;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.UuidGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @UuidGenerator
+    @GeneratedValue
+    UUID id;
+
+    @Column(name = "name", nullable = false)
+    String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    String email;
+
+    @Column(name = "password", nullable = false)
+    String password;
+
+    @Builder.Default
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_role", nullable = false)
+    UserRole userRole = UserRole.USER;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    Instant createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    Instant updatedAt;
+}
---
+++ src/main/java/az/bhos/teknofest/utils/enums/UserRole.java
@@ -0,0 +1,7 @@
+package az.bhos.teknofest.utils.enums;
+
+public enum UserRole {
+    USER,
+    ADMIN,
+    SUPER_ADMIN,
+}
