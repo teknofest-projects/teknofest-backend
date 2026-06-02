0c29889f4a2528c55798c2cc0fd3825ce14bc4a2
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-03 00:42:09 +0400

    first version from start.spring.io

---
+++ src/main/java/az/bhos/teknofest/TeknofestApplication.java
@@ -0,0 +1,13 @@
+package az.bhos.teknofest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknofestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeknofestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/bhos/teknofest/TeknofestApplicationTests.java
@@ -0,0 +1,13 @@
+package az.bhos.teknofest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknofestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
