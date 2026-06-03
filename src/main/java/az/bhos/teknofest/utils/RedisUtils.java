642ac7f7b83015567e36a45ff343ae510ff588de
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-03 21:54:44 +0400

    minor: small changes in codes

--- src/main/java/az/bhos/teknofest/utils/RedisUtils.java
+++ src/main/java/az/bhos/teknofest/utils/RedisUtils.java
@@ -6,30 +6,8 @@ public final class RedisUtils {
     }
 
     public static final String REGISTER_PREFIX = "auth:register:email:";
-    public static final String REGISTER_COOLDOWN = "auth:register:cooldown:";
-
-    public static final String RESET_TOKEN_PREFIX = "auth:reset:token:";
-    public static final String RESET_COOLDOWN_PREFIX = "auth:reset:cooldown:";
-
-    public static final String EMAIL_UPDATE_PREFIX = "auth:email-update:";
 
     public static String registerKey(String email) {
         return REGISTER_PREFIX + email;
-    }
-
-    public static String registerCooldownKey(String email) {
-        return REGISTER_COOLDOWN + email;
-    }
-
-    public static String resetTokenKey(String token) {
-        return RESET_TOKEN_PREFIX + token;
-    }
-
-    public static String emailUpdateKey(String email) {
-        return EMAIL_UPDATE_PREFIX + email;
-    }
-
-    public static String resetCooldownKey(String email) {
-        return RESET_COOLDOWN_PREFIX + email;
     }
 }
